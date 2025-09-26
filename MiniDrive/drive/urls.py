d9c606f805c9691b8295351aec3f5958beb9fc28
KARTIKNAIK18 <kartiknaik560@gmail.com>
2025-09-26 21:38:17 +0530

    added the api-gateway to list files

--- MiniDrive/drive/urls.py
+++ MiniDrive/drive/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home)
+    path('', views.Get_files)
     
 ]
--- MiniDrive/drive/views.py
+++ MiniDrive/drive/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+import requests
+from decouple import config
 
 # Create your views here.
+API_URL = config("API_URL")
 
-def Home(request):
-    return render(request, 'home.html')
+# def Home(request):
+#     return render(request, 'home.html')
 
 
-def Get_files():
-    pass
+def Get_files(request):
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        files = response.json()
+
+    except Exception as e :
+        files = []
+        print("Error fetching files",e)
+    return render(request, "home.html",{"files": files})
 
 def Put_Files():
     pass
