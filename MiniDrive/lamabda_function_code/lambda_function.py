b70d1163f0816a0a1ae7a15b2547f69a652cd793
KARTIKNAIK18 <kartiknaik560@gmail.com>
2025-10-15 23:45:32 +0530

    delete method added in the lambda function

--- MiniDrive/lamabda_function_code/lambda_function.py
+++ MiniDrive/lamabda_function_code/lambda_function.py
@@ -50,7 +50,28 @@ def lambda_handler(event, context):
                 "body": json.dumps({"error": str(e)})
             }
 
+    elif method == "DELETE":
+        try:
 
+            filename = event.get("filename")
+
+            if not filename:
+                return {
+                    "statusCode": 400,
+                    "body": json.dumps({"error": "Missing filename"})
+                }
+            response = s3.delete_object(Bucket=BUCKET_NAME, Key=filename)
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"message": f"File '{filename}' deleted successfully"})
+            }
+
+        
+        except Exception as e:
+            return {
+                "statusCode": 500,
+                "body": json.dumps({"error": str(e)})
+            }
 
     return {
         "statusCode": 400,
